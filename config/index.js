45ec8973a88cf5ea197e4d8e79bcd89dddfad26e
luguiqing <luguiqing@stargraph.cn>
2019-10-15 16:39:55 +0800

    想要上传gh-pages需要修改assetsPublicPath

--- config/index.js
+++ config/index.js
@@ -59,8 +59,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    // assetsPublicPath: '/',
-    assetsPublicPath: '/blog',
+    assetsPublicPath: '/',
+    // assetsPublicPath: '/blog/',
 
     /**
      * Source Maps
